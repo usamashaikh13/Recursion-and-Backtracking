62c669a08d7f5d72468ed8570f3550100672ccba
usamashaikh13 <73382214+usamashaikh13@users.noreply.github.com>
2022-02-20 15:37:27 +0530

    Create printzigzag.cpp

---
+++ printzigzag.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+
+void pzz(int n){
+    // write your code here
+    if(n==0) return;
+    cout<<n<<" ";
+    pzz(n-1);
+    cout<<n<<" ";
+    pzz(n-1);
+    cout<<n<<" ";  
+}
+
+int main(){
+    int n; cin>>n;
+    pzz(n);
+}
