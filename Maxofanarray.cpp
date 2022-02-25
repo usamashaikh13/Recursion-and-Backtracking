48241eb2520173a3f545607af20aeddad799eef3
usamashaikh13 <73382214+usamashaikh13@users.noreply.github.com>
2022-02-25 08:56:15 +0530

    Create Maxofanarray.cpp

---
+++ Maxofanarray.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int max(int arr[], int idx, int n){
+    // write your code here
+    if(idx==n-1) return arr[idx];
+    int misa = max(arr,idx+1,n);
+    if(misa>arr[idx]){
+        return misa;
+    }
+    else{
+        return arr[idx];
+    }
+    
+}
+
+int main(){
+    int n;
+    cin >> n;
+    int arr[n];
+    for (int i = 0; i < n; i++)
+        cin >> arr[i];
+    int p = max(arr, 0, n);
+    cout << p << endl;
+}
